436553bc60d40c1f470be9bb08ac3237a222f1c2
Sindre Wilting <sindrewilting@gmail.com>
2019-10-27 08:39:32 +0100

    Use YarnWorkspace construct in Project

--- Source/Project/Project.ts
+++ Source/Project/Project.ts
@@ -5,7 +5,7 @@
 import path from 'path';
 import glob from 'glob';
 import isGlob from 'is-glob';
-import { Package, ProjectSources } from "../internal";
+import { Package, ProjectSources, YarnWorkspace } from "../internal";
 
 /**
  * Represents a project
@@ -17,7 +17,7 @@ export class Project {
 
     private _root: string
     private _rootPackage: Package
-    private _workspacePackages: Package[] = []
+    private _workspaces: YarnWorkspace[] = []
     private _sources: ProjectSources
 
     /**
@@ -30,9 +30,10 @@ export class Project {
         this._rootPackage = new Package(this._root);
 
         if (this._rootPackage.hasWorkspaces()) {
-            this.createWorkspacePackages();
+            this.createWorkspaces();
         }
-        this._sources = new ProjectSources(this._root, this._workspacePackages);
+        
+        this._sources = new ProjectSources(this._root, this._workspaces.map(_ => _.workspacePackage));
     }
     /**
      * Gets the absolute path to the root folder
@@ -51,12 +52,12 @@ export class Project {
         return this._rootPackage;
     }
     /**
-     * Gets the the {Package} for each yarn workspace
+     * Gets the {YarnWorkspace} meta-data configuration for each yarn workspace in the project
      *
      * @readonly
      */
-    get workspacePackages() {
-        return this._workspacePackages;
+    get workspaces() {
+        return this._workspaces;
     }
     /**
      * Gets the {Sources} for this project
@@ -72,27 +73,27 @@ export class Project {
      * @returns
      */
     hasWorkspaces() {
-        return this._workspacePackages !== undefined && this._workspacePackages.length > 0;
+        return this._workspaces !== undefined && this._workspaces.length > 0;
     }
     
-    private createWorkspacePackages() {
-        this._workspacePackages = [];
+    private createWorkspaces() {
+        this._workspaces = [];
         let rootPackageObject = this._rootPackage.packageObject;
         rootPackageObject.workspaces!.forEach(workspace => {
             if (isGlob(workspace)) {
                 glob.sync(workspace, {absolute: true, }).forEach(workspacePath => {
                     try {
-                        this._workspacePackages.push(new Package(workspacePath, this._rootPackage));
+                        this._workspaces.push(new YarnWorkspace(new Package(workspacePath, this._rootPackage)));
                     } catch (error) {
-                        throw Error(`Could not create Package for workspace at path`);
+                        throw Error(`Could not create YarnWorkspace for workspace at path '${workspacePath}'`);
                     }
                 });
             }
             else {
                 try {
-                    this._workspacePackages.push(new Package(workspace, this._rootPackage));
+                    this._workspaces.push(new YarnWorkspace(new Package(workspace, this._rootPackage)));
                 } catch (error) {
-                    throw Error(`Could not create Package for workspace at path`);
+                    throw Error(`Could not create YarnWorkspace for workspace at path '${workspace}'`);
                 }
             }
         });
